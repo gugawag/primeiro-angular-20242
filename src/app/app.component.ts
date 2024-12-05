226ea58f5f8e7418ab605e42610df766d638f29c
Gustavo Wagner <gugawag@gmail.com>
2024-12-05 14:39:59 -0300

    primeiro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Aluno} from "./shared/modelo/aluno";
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'primeiro-angular-20242';
+  ALUNOS: Aluno[];
+  alunoDeCadastro: Aluno;
+
+  constructor() {
+    this.ALUNOS = new Array<Aluno>();
+    this.ALUNOS.push(new Aluno('123', 'Aluno 1', 20));
+    this.ALUNOS.push(new Aluno('223', 'Aluno 2', 21));
+    this.ALUNOS.push(new Aluno('323', 'Aluno 3', 25));
+
+    this.alunoDeCadastro = new Aluno();
+  }
+
+  cadastrar() {
+    this.ALUNOS.push(this.alunoDeCadastro);
+    this.alunoDeCadastro = new Aluno();
+  }
+
+  remover(alunoARemover: Aluno) {
+    this.ALUNOS = this.ALUNOS.filter(
+        aluno => aluno.matricula !== alunoARemover.matricula);
+  }
 }
