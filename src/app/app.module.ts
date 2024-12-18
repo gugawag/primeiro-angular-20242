0f6bea6ed0431dad1790097c922b8de63af4947d
Gustavo Wagner <gugawag@gmail.com>
2024-12-18 08:58:31 -0300

    v2-pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import { MatriculaPipe } from './shared/pipes/matricula.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatriculaPipe
   ],
     imports: [
         BrowserModule,
